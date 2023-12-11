c541232c45409b236768a39fc4d3e94e11fd5ac1
kolaider <nikolai.strom@skz.by>
2023-12-12 01:26:42 +0300

    9.3 POST request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Depends, HTTPException, status, Path, Query
 import models
 from models import Todo
 from database import engine, SessionLocal
+from pydantic import BaseModel, Field
 
 
 app = FastAPI()
@@ -21,6 +22,15 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class TodoRequest(BaseModel):
+
+    title: str = Field(min_length=3)
+    description: str = Field(max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
+
+
+
 @app.get("/")
 async def read_all(db: db_dependency):
     return db.query(Todo).all()
@@ -32,3 +42,10 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    todo_model = Todo(**todo_request.model_dump())
+    db.add(todo_model)
+    db.commit()
