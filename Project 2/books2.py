13e0d8e9b8eeea456e8769d8008ddc4bc8d04d0e
kolaider <nikolai.strom@skz.by>
2023-12-11 21:31:46 +0300

    6 6:pydantic book request validation

--- Project 2/books2.py
+++ Project 2/books2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -6,12 +7,26 @@ app = FastAPI()
 
 class Book:
 
-    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
-        self.id: int = id
-        self.title: str = title
-        self.author: str = author
-        self.description: str = description
-        self.rating: int = rating
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+    def __init__(self, id, title, author, description, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+
+
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
 
 
 BOOKS = [
@@ -31,5 +46,6 @@ async def get_books():
 
 
 @app.post("/create_book")
-async def create_book(book_request=Body()):
-    BOOKS.append(book_request)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(new_book)
