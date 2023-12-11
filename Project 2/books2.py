c34f3473ab5ca840866baf6503b4f9cbb9a243d1
kolaider <nikolai.strom@skz.by>
2023-12-11 23:10:12 +0300

    6.17 Data validation Query Parameters

--- Project 2/books2.py
+++ Project 2/books2.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, Path
+from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel, Field
 
 app = FastAPI()
@@ -67,7 +67,7 @@ async def read_book(book_id: int = Path(gt=0)):
 
 
 @app.get("/books/")
-async def read_book_by_rating(book_rating: int):
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
     books_to_return = []
     for book in BOOKS:
         if book.rating == book_rating:
@@ -87,7 +87,7 @@ def find_book_id(book: Book):
 
 
 @app.get("/books/publish/")
-async def read_books_by_publish_date(publish_date: int):
+async def read_books_by_publish_date(publish_date: int = Query(gt=1999, lt=2031)):
     books_to_return = []
     for book in BOOKS:
         if book.published_date == publish_date:
