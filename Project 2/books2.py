904bd1a082de2603d1add92a64a70ff23c02729b
kolaider <nikolai.strom@skz.by>
2023-12-11 22:50:24 +0300

    6.13 Delete Book with DELETE method

--- Project 2/books2.py
+++ Project 2/books2.py
@@ -88,3 +88,11 @@ async def update_book(book: BookRequest):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if book_id == BOOKS[i].id:
+            BOOKS.pop(i)
+            break
